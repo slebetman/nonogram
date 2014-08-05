e57db92a517657b18a42ba7baefc7870318b7c97
Adly Abdullah <adly_abdullah@cention.asia>
2014-08-05 16:10:58 +0800

    Properly implemented calculating row permutations

---
+++ nonorow.tcl
@@ -0,0 +1,41 @@
+#! /usr/bin/env tclsh
+
+proc sum {numbers} {
+	expr [join $numbers +]
+}
+
+proc split_row {n m} {
+	if {$m <= 1} {
+		return $n
+	} else {
+		set result {}
+		for {set x 1} {$x <= ($n-$m+1)} {incr x} {
+			foreach y [split_row [expr $n-$x] [expr $m-1]] {
+				lappend result [list $x {*}$y]
+			}
+		}
+		return $result
+	}
+}
+
+proc process_row {n m} {
+	set result {}
+	foreach x [split_row [expr $n+2] $m] {
+		lset x 0 [expr [lindex $x 0]-1]
+		lset x end [expr [lindex $x end]-1]
+		lappend result $x
+	}
+	return $result
+}
+
+proc nonorow {ngram row} {
+	set row_remainder [expr {$row-([sum $ngram]+[llength $ngram]-1)}]
+	
+	if {$row_remainder < 0} {
+		error "Row not large enough to contain nonogram!"
+	}
+	
+	
+}
+
+puts [join [process_row 5 3] \n]
